d7ff10f0fad6ed84eeb75c192ce6e31550d6c7d5
alisa <alisar17112006@gmail.com>
2024-04-25 12:29:17 +0300

    mod_main_add_1

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hello world!")
+import random
+import math
+lst1 = []
+lst2 = []
+size = int(input('size lst1 = '))
+for i in range(size):
+    temp = int(input('=> '))
+    lst1.append(temp)
+
+size = int(input('size lst1 = '))
+for i in range(size):
+    temp = int(input('=> '))
+    lst1.append(temp)
+
+for i in lst1:
+    if i % 2 != 0:
+        lst2.append(i)
+print(lst2)
